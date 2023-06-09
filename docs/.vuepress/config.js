158325400b43ce02b579e7314385f2819961656b
gclhaha <gclhaha@qq.com>
2023-06-10 01:38:20 +0800

    feat: add google ad

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,4 +3,14 @@ import { defineUserConfig } from 'vuepress'
 export default defineUserConfig({
   lang: 'zh-CN',
   title: 'gclhahah的博客',
+  head: [
+    [
+      "script",
+      {
+        "data-ad-client": "ca-pub-6298430787430832",
+        async: true,
+        src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      }
+    ]
+  ]
 })
